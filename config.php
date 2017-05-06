31651f83324af0b417149ad85fe4148223be4210
Kirill Romanov <kirillik95@yandex.ru>
2017-05-06 18:41:02 +0300

    + config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$config = array(
+    'title' => 'Notes Web Application - Developed by Kirill Romanov',   //заголовок во вкладке
+    'head_title' => 'Notes Web Service',                                //заголовок в меню
+    'show_limit' => 5,                                                  //отображаемое число записей на странице
+    'db' => array(                                                      //данные для базы данных
+        'host' => 'localhost',                                          //хост
+        'user' => 'kirban',                                             //пользователь
+        'password' => '34752',                                          //пароль
+        'dbname' => 'practice'),                                        //имя БД
+);
